317df71b0859fd49db5f863606e6e49562042581
RishabhJain2018 <rishabhjain2018@gmail.com>
2017-03-14 23:16:40 +0530

    Profiles: Added Forms and migrations.

---
+++ registration/profiles/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import UserDetail
+from django.contrib.auth.models import User
+
+
+class UserForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
+        exclude = ['username']
+
+
+class UserDetailForm(forms.ModelForm):
+
+    class Meta:
+        model = UserDetail
+        exclude = ['User']
--- registration/registration/settings.py
+++ registration/registration/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'regis',
     'import_export',
+    'profiles',
 )
 
 MIDDLEWARE_CLASSES = (
