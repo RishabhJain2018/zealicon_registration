b2e0af5d5b3e16659108ef3d53a884e559ea5a8b
RishabhJain2018 <rishabhjain2018@gmail.com>
2017-03-14 23:17:51 +0530

    Profiles: Added group Permissions

---
+++ registration/profiles/permissions.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import Group
+
+
+def is_in_group(user, group_name):
+    """
+    Takes a user and a group name, and returns `True` if the user is in that group.
+    """
+    return Group.objects.get(name=group_name).user_set.filter(id=user.id).exists()
