152d11908b9224a8c41e1acda3ef367f6978db26
RishabhJain2018 <rishabhjain2018@gmail.com>
2017-03-15 19:56:04 +0530

    User Profile Admin Added.

--- registration/profiles/admin.py
+++ registration/profiles/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from profiles.models import UserDetail
+from import_export.admin import ImportExportModelAdmin
 
-# Register your models here.
+
+class UserDetailAdmin(ImportExportModelAdmin):
+    list_display = ('user','contact_no','college_society','year','branch','univ_roll_no',)
+    list_filter = ('college_society', 'branch')
+    search_fields = ('user__username', 'contact_no', 'univ_roll_no', 'year', 'college_society',)
+
+
+admin.site.register(UserDetail, UserDetailAdmin)
